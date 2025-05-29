234c0f7fcac5f210b5c37e2d49a9fced1160c56f
Huzaifa <huzaifaar2003@gmail.com>
2025-05-29 12:53:11 +0300

    Send email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
-
+from send_email import send_mail
 API_KEY = "31a91e4e6dd24e138b4c7edb7b32e305"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2025-04-28&sortBy=publishedAt&apiKey=31a91e4e6dd24e138b4c7edb7b32e305"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2025-04-29&sortBy=publishedAt&apiKey=31a91e4e6dd24e138b4c7edb7b32e305"
 
-# adding a headers varible to put inot the requests.get() as the "headers" argument value
+# adding a headers variable to put into the requests.get() as the argument for the "headers" parameter
 # did that since I was getting a return of "Edge: Too Many Requests"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -11,11 +11,28 @@ headers = {
 }
 
 # make request
-request = requests.get(url, headers=headers)
+request = requests.get(url)
+
 # extract the content from the url in a .json format so that it is more usable
 content = request.json()
+print(content,"\n")
 
-for article in content["articles"]:
-    print(article["title"])
-    print(article["content"])
-    print("\n")
+
+
+# extracting top 5 stories' information
+message = []
+body = ""
+for i in range(0,5):
+    article = content["articles"][i]
+
+    body = body + article["title"] + "\n"  + article["description"] + "\n\n"
+
+
+
+print("Str: \n",body)
+
+complete_message = body.encode('utf-8')
+# convert string to unicode by using
+# .encode() method for strings
+
+send_mail(message = complete_message    , username= "automated.huzaifaar2003@gmail.com")
---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib
+import ssl
+
+
+def send_mail(message, username= "automated.huzaifaar2003@gmail.com"):
+    host = "smtp.gmail.com"  # NOT "smtp@gmail.com"
+    port = 465
+    #username = "automated.huzaifaar2003@gmail.com"
+    password = "ymtdpvmwkqpolebh"
+    receiver = "automated.huzaifaar2003@gmail.com"
+    context = ssl.create_default_context()
+    #message = message
+    with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
+        # explicit declaration of "context" argument because argument order wasn't followed
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
